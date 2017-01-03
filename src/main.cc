a33d953b1c4fca8b1c9b5358d93437e574a0b32d
fn.stanc <fn.stanc@gmail.com>
2017-01-03 11:10:49 +0800

    Use getopt to deal with commandline args.

--- src/main.cc
+++ src/main.cc
@@ -6,12 +6,46 @@
  */
 
 #include "plugin_manager_impl.h"
+#include "getopt.h"
+#include <string>
+#include <cstdio>
+
+void usage()
+{
+    fprintf(stdout, "This is Usage.\n");
+}
 
 int main(int argc, char *argv[])
 {
     using namespace yatl;
     PluginManagerImpl pm;
 
+    std::string conf_file;
+
+    int ch;
+    while ((ch = getopt(argc, argv, "c:h")) != -1) {
+        switch (ch) {
+        case 'c': {
+            conf_file = optarg;
+            break;
+        }
+        case 'h': {
+            usage();
+            return 0;
+        }
+        default: {
+            fprintf(stderr, "Unknown arg %d.\n", ch);
+            return -1;
+        }
+        }
+    }
+
+    if (conf_file.empty())
+    {
+        fprintf(stderr, "Need a config file.\n");
+        usage();
+        return -2;
+    }
 #if defined(_WIN32) || defined(_WIN64)
     if (!pm.init("../config_win.json")) {
 #else
